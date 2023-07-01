b18ffc524d0a7e4940f9089264bc60fb8e495b42
1234K4321 <79265150+1234K4321@users.noreply.github.com>
2023-07-01 20:42:41 +0330

    Update code_gen.py

--- code_gen.py
+++ code_gen.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import List
 from itertools import count
+from collections import OrderedDict
 
 PB_SIZE = 1000
 
@@ -31,28 +32,34 @@ def print_str(a):
 
 class SYMBOL_TABLE_KEYS(str, Enum):
     ADDRESS = 'address'
-    ALLOCATED = 'allocated'
-    # TYPE = 'type'
+
 
 class FUNC_TABLE_KEYS(str, Enum):
     CALL_ADDRS = 'CALL_ADDRS'
     FUNC_ADDR = 'FUNC_ADDR'
-    PARAMS_ADDRS = 'PARAMS_ADDRS'
     RETURN_ADDR = 'RETURN_ADDR'
     RETURN_VAL = 'RETURN_VAL'
+    PARAMS_DICT = 'PARAMS_DICT'
+    ARG_COUNT = 'ARG_COUNT'
+
+
+class PARAM_DICT_KEYS(str, Enum):
+    ADDRESS = 'address'
 
 
 class FuncTable:
     # func_id = str(self.symbol_table[func_name][SYMBOL_TABLE_KEYS.ADDRESS])
 
     def __init__(self):
-        self.dict = dict()  # {func_id: {FUNC_TABLE_KEYS.CALL_ADDRS: [], FUNC_TABLE_KEYS.FUNC_ADDR: func_addr}}
+        self.dict = dict()  # {func_id: {FUNC_TABLE_KEYS.CALL_ADDRS: [], FUNC_TABLE_KEYS.FUNC_ADDR: func_addr, ...}}
 
     def exists(self, func_id):
         return func_id in self.dict
 
     def insert(self, func_id):
-        self.dict[func_id] = {FUNC_TABLE_KEYS.CALL_ADDRS: [], FUNC_TABLE_KEYS.PARAMS_ADDRS: []}
+        self.dict[func_id] = {FUNC_TABLE_KEYS.CALL_ADDRS: [],
+                              FUNC_TABLE_KEYS.PARAMS_DICT: OrderedDict(),  # {sym_table addr: dict()}
+                              FUNC_TABLE_KEYS.ARG_COUNT: 0}
 
     def append_call_addr(self, func_id, addr):
         self.dict[func_id][FUNC_TABLE_KEYS.CALL_ADDRS] = self.dict[func_id][FUNC_TABLE_KEYS.CALL_ADDRS] + [addr]
@@ -61,14 +68,22 @@ class FuncTable:
         # set i in PB (start of func in PB)
         self.dict[func_id][FUNC_TABLE_KEYS.FUNC_ADDR] = func_addr
 
-    def append_param_addr(self, func_id, param_addr):
-        self.dict[func_id][FUNC_TABLE_KEYS.PARAMS_ADDRS] = self.dict[func_id][FUNC_TABLE_KEYS.PARAMS_ADDRS] + [param_addr]
-
     def set_return_addr(self, func_id, return_addr):
         self.dict[func_id][FUNC_TABLE_KEYS.RETURN_ADDR] = return_addr
 
     def set_return_val(self, func_id, return_val):
         self.dict[func_id][FUNC_TABLE_KEYS.RETURN_VAL] = return_val
+
+    def add_param_var(self, func_id: str, param_addr: str, addr_counter):
+        self.dict[func_id][FUNC_TABLE_KEYS.PARAMS_DICT][param_addr] = \
+            {PARAM_DICT_KEYS.ADDRESS: str(next(addr_counter))}
+
+    def add_param_arr(self, func_id: str, param_addr: str, addr_counter):
+        self.dict[func_id][FUNC_TABLE_KEYS.PARAMS_DICT][param_addr] = \
+            {PARAM_DICT_KEYS.ADDRESS: '@' + str(next(addr_counter))}
+
+    def has_sym_table_addr(self, func_id, sym_table_addr):
+        return sym_table_addr in self.dict[func_id][FUNC_TABLE_KEYS.PARAMS_DICT]
 
 
 class CodeGen:
@@ -99,18 +114,40 @@ class CodeGen:
         self.func_table.set_func_addr(self.func_id[-1], self.i)
         self.func_table.set_return_addr(self.func_id[-1], next(self.addr_counter))
         self.func_table.set_return_val(self.func_id[-1], next(self.addr_counter))
+        ret_val = self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.RETURN_VAL]
+        self.pb[self.i] = assign_str('#0', ret_val)
+        self.i += 1
         if self.is_main():
             self.pb[0] = jp_str(self.i)
         self.ss.pop()
 
     def is_main(self):
         return ('main' in self.symbol_table) and \
-            (str(self.symbol_table['main'][SYMBOL_TABLE_KEYS.ADDRESS]) == self.func_id[-1])
+            (str(self.symbol_table['main'][SYMBOL_TABLE_KEYS.ADDRESS]) == self.func_id[0])
 
-    def add_param(self):
+    def add_param_var(self):
         param_addr = self.ss[-1]
-        self.func_table.append_param_addr(self.func_id[-1], param_addr)
+        self.func_table.add_param_var(self.func_id[-1], param_addr, self.addr_counter)
         self.ss.pop()
+
+    def add_param_arr(self):
+        param_addr = self.ss[-1]
+        self.func_table.add_param_arr(self.func_id[-1], param_addr, self.addr_counter)
+        self.ss.pop()
+
+    def add_local_var(self):
+        if len(self.func_id) > 0:
+            var_addr = self.ss[-1]
+            self.func_table.add_param_var(self.func_id[-1], var_addr, self.addr_counter)
+            self.ss.pop()
+            self.ss.append(self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.PARAMS_DICT][var_addr][PARAM_DICT_KEYS.ADDRESS])
+
+    def add_local_arr(self):
+        if len(self.func_id) > 0:
+            var_addr = self.ss[-1]
+            self.func_table.add_param_arr(self.func_id[-1], var_addr, self.addr_counter)
+            self.ss.pop()
+            self.ss.append(self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.PARAMS_DICT][var_addr][PARAM_DICT_KEYS.ADDRESS])
 
     def pop_func_id(self):
         self.func_id.pop()
@@ -122,25 +159,34 @@ class CodeGen:
         self.func_id.append(self.ss[-1])
         self.func_table.append_call_addr(self.func_id[-1], self.i)
         self.ss.pop()
-        self.arg_count = 0
+        self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.ARG_COUNT] = 0
 
     def call_after_args(self):
         self.pb[self.i] = assign_str('#' + str(self.i + 2), self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.RETURN_ADDR])
         self.i += 1
         self.pb[self.i] = jp_str(self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.FUNC_ADDR])
         self.i += 1
-        self.ss.append(str(self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.RETURN_VAL]))
+        t = next(self.addr_counter)
+        ret_val = self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.RETURN_VAL]
+        self.pb[self.i] = assign_str(ret_val, t)
+        self.i += 1
+        self.ss.append(str(t))
         self.pop_func_id()
 
     def add_arg(self):
+        arg_count = self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.ARG_COUNT]
         expression = self.ss[-1]
-        self.pb[self.i] = assign_str(expression,
-                                     self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.PARAMS_ADDRS][self.arg_count])
+        param_addr = list(self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.PARAMS_DICT].values())[arg_count][PARAM_DICT_KEYS.ADDRESS]
+        if param_addr[0] == '@':
+            self.pb[self.i] = assign_str('#' + str(expression), param_addr[1:])
+        else:
+            self.pb[self.i] = assign_str(str(expression), param_addr)
         self.i += 1
-        self.arg_count += 1
+        self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.ARG_COUNT] = \
+            self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.ARG_COUNT] + 1
         self.ss.pop()
 
-    def return_(self):
+    def ret_jump(self):
         if not self.is_main():
             self.pb[self.i] = jp_str('@' + str(self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.RETURN_ADDR]))
             self.i += 1
@@ -149,51 +195,34 @@ class CodeGen:
         self.pb[self.i] = assign_str(self.ss[-1], self.func_table.dict[self.func_id[-1]][FUNC_TABLE_KEYS.RETURN_VAL])
         self.i += 1
         self.ss.pop()
-        self.return_()
+        self.ret_jump()
 
+    def get_local_addr_if_in_func(self, sym_table_addr: str):
+        if len(self.func_id) > 0 and self.func_table.has_sym_table_addr(self.func_id[0], sym_table_addr):
+            return True, str(self.func_table.dict[self.func_id[0]][FUNC_TABLE_KEYS.PARAMS_DICT][sym_table_addr][PARAM_DICT_KEYS.ADDRESS])
+        else:
+            return False, sym_table_addr
 
     def pid(self):
         id_lexeme = self.parser.current_token[1]
-        p = self.symbol_table[id_lexeme][SYMBOL_TABLE_KEYS.ADDRESS]
-        self.ss.append(str(p))
+        p = str(self.symbol_table[id_lexeme][SYMBOL_TABLE_KEYS.ADDRESS])
+        p = self.get_local_addr_if_in_func(p)[1]
+        self.ss.append(p)
 
     def pnum(self):
         num_lexeme = self.parser.current_token[1]
         self.ss.append(str('#' + str(num_lexeme)))
 
     def allocate_var(self):
-        for key in self.symbol_table:
-            if SYMBOL_TABLE_KEYS.ADDRESS in self.symbol_table[key] and \
-                    self.symbol_table[key][SYMBOL_TABLE_KEYS.ADDRESS] == int(self.ss[-1]):
-                self.symbol_table[key][SYMBOL_TABLE_KEYS.ALLOCATED] = True
         self.pb[self.i] = assign_str('#0', self.ss[-1])
         self.i += 1
         self.ss.pop()
 
-    def allocate_var_assign(self):
-        already_allocated = False
-        for key in self.symbol_table:
-            if SYMBOL_TABLE_KEYS.ADDRESS in self.symbol_table[key] and \
-                    self.symbol_table[key][SYMBOL_TABLE_KEYS.ADDRESS] == int(self.ss[-1]):
-                if self.symbol_table[key][SYMBOL_TABLE_KEYS.ALLOCATED]:
-                    already_allocated = True
-                else:
-                    self.symbol_table[key][SYMBOL_TABLE_KEYS.ALLOCATED] = True
-        if not already_allocated:
-            self.pb[self.i] = assign_str('#0', self.ss[-1])
-            self.i += 1
-
     def allocate_array(self):
-        for key in self.symbol_table:
-            if SYMBOL_TABLE_KEYS.ADDRESS in self.symbol_table[key] and \
-                    self.symbol_table[key][SYMBOL_TABLE_KEYS.ADDRESS] == int(self.ss[-2]):
-                self.symbol_table[key][SYMBOL_TABLE_KEYS.ALLOCATED] = True
         self.pb[self.i] = assign_str('#0', self.ss[-2])
         arr_len = int(self.ss[-1][1:])
         for _ in range(arr_len - 1):
             next(self.addr_counter)
-            # self.pb[self.i] = assign_str('#0', next(self.addr_counter))
-            # self.i += 1
         self.ss.pop()
         self.ss.pop()
         self.i += 1
